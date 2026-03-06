115dc705bb92111c482db88833e5dbe120500d09
Abhirup Roy <abhiruproy2002@gmail.com>
2026-03-06 01:21:04 +0000

    Update dependencies in pyproject.toml and add pyrocky to __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -9,7 +9,14 @@ Author: Abhirup Roy
 
 __version__ = "0.1"
 __author__ = "Abhirup Roy"
-__all__ = ["launch_sweep", "launch_ofat", "analyse", "externals", "launch_sobol"]
+__all__ = [
+    "launch_sweep",
+    "launch_ofat",
+    "analyse",
+    "externals",
+    "launch_sobol",
+    "pyrocky",
+]
 
 
 from .doe.sweep import launch_sweep
@@ -17,3 +24,4 @@ from .doe.ofat import launch_ofat
 from .doe.sobol import launch_sobol
 from . import sweep_analysis as analyse
 from . import externals
+from . import pyrocky
